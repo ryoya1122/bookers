6bebff85de0ee6dd0f45b73dd388862c31cfefaa
ryoya1122 <koba.jht@gmail.com>
2020-01-08 20:18:36 +0900

    [Start] task2

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,43 @@
+class BooksController < ApplicationController
+  def index
+  	@book = Book.new
+  	@books = Book.all
+  end
+
+  def show
+  	@book = Book.find(params[:id])
+  end
+
+  def new
+  end
+
+  def create
+  	book = Book.new(book_params)
+  	book.save
+  	flash[:notice] ="Book was successfully created."
+  	redirect_to book_path(book)
+  end
+
+  def edit
+  	@book = Book.find(params[:id])
+  end
+
+  def update
+  	book = Book.find(params[:id])
+  	book.update(book_params)
+  	flash[:notice] ="Book was successfully updated."
+  	redirect_to book_path(book)
+  end
+
+  def destroy
+  	book = Book.find(params[:id])
+  	book.destroy
+  	flash[:notice] ="Book was successfully destroyed."
+  	redirect_to books_path
+  end
+  private
+
+  def book_params
+  	params.require(:book).permit(:title, :body)
+  end
+end
